8460628f90055de99598b6756b55a98e59c219b6
TomoyaKitaura <kitaura@supinf.co.jp>
2020-04-20 11:35:41 +0900

    initialized repo

--- tools/init_repo.sh
+++ tools/init_repo.sh
@@ -2,7 +2,6 @@
 set -eu
 
 export LOADBALANCER_IP_ADDRESS=$(gcloud compute addresses list --filter="name=testbedip" --format="value(address)")
-export TB_GITHUB_ORG_NAME=TomoyaKitaura
 
 CURRENT_DIR=$(cd $(dirname $0); pwd)
 
@@ -11,11 +10,11 @@ git checkout develop
 for DIR in manifests microservices; do
   for FILE in `find ${CURRENT_DIR}/../${DIR}/ -type f`; do
     perl -pi -e "s|__LOADBALANCER_IP_ADDRESS__|${LOADBALANCER_IP_ADDRESS}|g" $FILE;
-    perl -pi -e "s|__TB_GITHUB_ORG_NAME__|${TB_GITHUB_ORG_NAME}|g" $FILE;
+    perl -pi -e "s|__TB_GITHUB_ORG_NAME__|${TB_GITHUB_USER}|g" $FILE;
   done
 done
 perl -pi -e "s|__LOADBALANCER_IP_ADDRESS__|${LOADBALANCER_IP_ADDRESS}|g" ${CURRENT_DIR}/../README.md
-perl -pi -e "s|__TB_GITHUB_ORG_NAME__|${TB_GITHUB_ORG_NAME}|g" ${CURRENT_DIR}/../README.md
+perl -pi -e "s|__TB_GITHUB_ORG_NAME__|${TB_GITHUB_USER}|g" ${CURRENT_DIR}/../README.md
 
 git add --all
 git commit -m "initialized repo"
